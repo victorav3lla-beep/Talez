c44eb568cff05e5b0c7c6e2b83e5b371999e883c
Victor Avella <victorhugo.avellalemus@gmail.com>
2025-12-02 12:46:22 +0100

    added index and show actions, and the index view for profiles

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -1,2 +1,19 @@
 class ProfilesController < ApplicationController
+  before_action :authenticate_user!
+
+  @user = current_user
+  def index
+    @profiles = Profile.all(@user)
+  end
+
+  def show
+    @profile = Profile.find(params[:id])
+  end
+
+
+  private
+
+  def playlist_params
+    params.require(:profile).permit(:name, :age, :username)
+  end
 end
