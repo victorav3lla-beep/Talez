eb1ed88ba24e753acc87d7516d47205908c456c5
Victor Avella <victorhugo.avellalemus@gmail.com>
2025-12-02 14:57:00 +0100

    finished the index page

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -1,20 +1,34 @@
 class ProfilesController < ApplicationController
   before_action :authenticate_user!
 
-  @user = current_user
+  # @user = current_user
   def index
-    @profiles = Profile.all(@user)
-    @profiles = @user.profiles
+    @profiles = Profile.all
+    # @profiles = @user.profiles
   end
 
   def show
     @profile = Profile.find(params[:id])
   end
 
+  def new
+    @user = current_user
+    @profile = Profile.new
+  end
+
+  def create
+    @profile = current_user.profiles.new(profile_params)
+    if @profile.save
+      redirect_to profile_path(@profile), notice: "Profile was successfully created! WOOOOO!!🎉🎉"
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
 
   private
 
-  def playlist_params
+  def profile_params
     params.require(:profile).permit(:name, :age, :username)
   end
 end
