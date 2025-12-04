8fca7d6018ec5f75e090a8de3a377bdfe75e2e57
Victor Avella <victorhugo.avellalemus@gmail.com>
2025-12-04 12:26:41 +0100

    doing smth for chats

--- app/controllers/stories_controller.rb
+++ app/controllers/stories_controller.rb
@@ -1,2 +1,27 @@
 class StoriesController < ApplicationController
+  before_action :authenticate_user!
+
+  def show
+    @story = current_user.stories.find(params[:id])
+  end
+
+  def new
+    @story = current_user.stories.build
+  end
+
+  def create
+    @story = current_user.stories.build(story_params)
+
+    if @story.save
+      redirect_to story_path(@story), notice: "Story created successfully!"
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def story_params
+    params.require(:story).permit(:title, :universe_id)
+  end
 end
