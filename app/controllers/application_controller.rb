667b2a132f3c628cf1b1f4c832a90a13b3e81a4a
David Pinheiro <david.pinheiro.d@gmail.com>
2026-01-14 23:31:27 +0100

    Fix devise 422 and add user fields

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,16 @@
 class ApplicationController < ActionController::Base
   before_action :authenticate_user!
 
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
   private
+
+  def configure_permitted_parameters
+    # Permit the `username` parameter along with the other
+    # sign up parameters.
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:username, :first_name, :last_name])
+    devise_parameter_sanitizer.permit(:account_update, keys: [:username, :first_name, :last_name])
+  end
 
   def current_profile
     @current_profile ||= Profile.find_by(id: session[:current_profile_id]) if session[:current_profile_id]
